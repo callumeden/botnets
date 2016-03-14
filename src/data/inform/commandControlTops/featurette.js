a9d505d13805035d0d6a13d227240100a64d7607
mjg15 <mjg15@ic.ac.uk>
2016-03-14 16:08:47 +0000

    Pictures for Michael's section done

--- src/data/inform/commandControlTops/featurette.js
+++ src/data/inform/commandControlTops/featurette.js
@@ -3,19 +3,22 @@ var JSON = [
         heading: "Centralised Server",
         subHeading: "Star Topologies",
         centralisedServer1: true,
-        image: "",
+        image: "img/Star.jpg",
+        imageRef: "",
         id: ""
     },
     {
         heading: "Centralised Server",
         subHeading: "Multi-server Topologies",
-        centralisedServer2: true
+        centralisedServer2: true,
+        image: "img/Multi.jpg"
     },
     {
         heading: "Hierarchical",
         subHeading: "",
         hierarchical: true,
         id: "hierarchical"
+        image: "img/Hierarchical.jpg"
     },
 
     {
@@ -31,6 +34,8 @@ var JSON = [
         subHeading: "Peer to Peer",
         p2p: true,
         id: "p2p"
+        image: "img/P2P.jpg",
+        imageRef: "Zeidanloo HR, Manaf  a. a. Botnet Command and Control Mechanisms. 2009 Second Int Conf Comput Electr Eng [Internet]. 2009;1:564-8. Available from: http://ieeexplore.ieee.org/lpdocs/epic03/wrapper.htm?arnumber=5380180"
     }
 
 ];
--- src/data/inform/communicationProtocols/featurette.js
+++ src/data/inform/communicationProtocols/featurette.js
@@ -4,17 +4,23 @@ var JSON = [
         subHeading: "Internet Relay Chat",
         irc: true,
         id: "irc"
+        image: "IRC.jpg"
+        imageRef: "Cooke E, Jahanian F, McPherson D. The zombie roundup: Understanding, detecting, and disrupting botnets. Networks [Internet]. 2005;7:39-44. Available from: http://www.usenix.org/events/sruti05/tech/full_papers/cooke/cooke_html"
     },
     {
         heading: "HTTP",
         subHeading: "HyperText Transfer Protocol",
         hyperText: true,
         id: "http"
+        image: "HTTP.jpg"
+        imageRef: "Zeidanloo HR, Manaf  a. a. Botnet Command and Control Mechanisms. 2009 Second Int Conf Comput Electr Eng [Internet]. 2009;1:564-8. Available from: http://ieeexplore.ieee.org/lpdocs/epic03/wrapper.htm?arnumber=5380180"
     },
     {
         heading: "Fluxing",
         fluxing: true,
         id: "fluxing"
+        image: "Flux.jpg"
+        imageRef: "Riden J. Fast flux web diagram [Internet]. 2008 [cited 14/03/2016] Available from: https://www.honeynet.org/node/133"
     }
 
 ];
